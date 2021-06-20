531bee5f80c072c9ce735878300badfa8fba0652
Martin <martinsung00@gmail.com>
2021-06-20 01:28:26 -0700

    Implemented about-me, badges, header, intro, portfolio, profile-card and skillset components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { SkillsetComponent } from './skillset/skillset.component';
+import { BadgesComponent } from './badges/badges.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, IntroComponent, ProfileCardComponent, AboutMeComponent, PortfolioComponent, SkillsetComponent, BadgesComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
